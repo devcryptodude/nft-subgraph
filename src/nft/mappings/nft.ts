2dc17bb98e98504efc3bc2130b749c5ac2d1b37b
DevCryptoDude <devcryptodude@gmail.com>
2021-12-05 21:10:41 +0100

    v1

--- src/nft/mappings/nft.ts
+++ src/nft/mappings/nft.ts
@@ -1,70 +1,92 @@
 /* eslint-disable prefer-const */
 import { BigInt, BigDecimal, store, Address } from '@graphprotocol/graph-ts'
 import {
- NFTContract
+ NFTContract,
+ METADATA
 } from '../../../generated/schema'
-import { NFT, Mint, NameChange, Transfer, BioChange} from '../../../generated/NFT/NFT'
+import { NFT, Mint, NameChange, Transfer, ResetStain, EmitRand} from '../../../generated/NFT/NFT'
 
 let ZERO_BI = BigInt.fromI32(0);
 let ONE_BI = BigInt.fromI32(1);
-
-export function handleMint(event: Mint): void {
-
-  let NFTID =  event.transaction.hash.toHex() + '-' + event.logIndex.toString()
-
-  let NFTC = getNFTAddress(event.address)
-
-  let value =  BigInt.fromI32(event.params.value.toI32())
-  let redemptionCount = BigInt.fromI32(event.params._redemptionCount.toI32())
-
-  let mint = new NFTContract(NFTID)
-  mint.tokenId = redemptionCount
-  mint.owner = NFTC.ownerOf(redemptionCount)
-  mint.metadata = NFTC.metadata(redemptionCount)
-  mint.save()
-
-}
 
 function getNFTAddress(address: Address): NFT {return NFT.bind(address)
 }
 
 
+
 export function handleNameChange(event: NameChange): void {
 
-  let NFTID =  event.transaction.hash.toHex() + '-' + event.logIndex.toString()
   let NFTC = getNFTAddress(event.address)
 
-  let name = new NFTContract(NFTID)
+  let timestamp = event.block.timestamp.toI32()
+
+  let name = new NFTContract(event.params.tokenId.toString())
+  name.date = BigInt.fromI32(timestamp)
+
   name.tokenId = BigInt.fromI32(event.params.tokenId.toI32())
   name.owner = NFTC.ownerOf(name.tokenId)
   name.metadata = NFTC.metadata(name.tokenId)
+
+  name.Stain = NFTC.getStain(name.tokenId)
+  name.named = NFTC.getnamed(name.tokenId)
+  name.mef = NFTC.getmef(name.tokenId)
+
   name.save()
+
+  let md = new METADATA(event.params.tokenId.toString())
+  md.number = NFTC.randomNumberID(name.tokenId)
+  md.save()
+
 }
 
 
-export function handleBioChange(event: BioChange): void {
 
-  let NFTID =  event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+export function handleResetStain(event: ResetStain): void {
+
   let NFTC = getNFTAddress(event.address)
 
-  let name = new NFTContract(NFTID)
-  name.tokenId = BigInt.fromI32(event.params.tokenId.toI32())
-  name.owner = NFTC.ownerOf(name.tokenId)
-  name.metadata = NFTC.metadata(name.tokenId)
-  name.save()
-}
+  let timestamp = event.block.timestamp.toI32()
 
+  let name = new NFTContract(event.params.tokenId.toString())
+
+  name.date = BigInt.fromI32(timestamp)
+  name.tokenId = BigInt.fromI32(event.params.tokenId.toI32())
+  name.metadata = NFTC.metadata(name.tokenId)
+  name.owner = NFTC.ownerOf(name.tokenId)
+
+  name.Stain = NFTC.getStain(name.tokenId)
+  name.named = NFTC.getnamed(name.tokenId)
+  name.mef = NFTC.getmef(name.tokenId)
+
+
+  name.save()
+
+  let md = new METADATA(event.params.tokenId.toString())
+  md.number = NFTC.randomNumberID(name.tokenId)
+  md.save()
+}
 
 export function handleTransfer(event: Transfer): void {
 
-  let NFTID =  event.transaction.hash.toHex() + '-' + event.logIndex.toString()
   let NFTC = getNFTAddress(event.address)
 
-  let name = new NFTContract(NFTID)
+  let timestamp = event.block.timestamp.toI32()
+  let name = new NFTContract(event.params.tokenId.toString())
+
+  name.date = BigInt.fromI32(timestamp)
   name.tokenId = BigInt.fromI32(event.params.tokenId.toI32())
-  name.owner = NFTC.ownerOf(name.tokenId)
   name.metadata = NFTC.metadata(name.tokenId)
+  name.owner = NFTC.ownerOf(name.tokenId)
+
+  name.Stain = NFTC.getStain(name.tokenId)
+  name.named = NFTC.getnamed(name.tokenId)
+  name.mef = NFTC.getmef(name.tokenId)
+
   name.save()
+
+  let md = new METADATA(event.params.tokenId.toString())
+  md.number = NFTC.randomNumberID(name.tokenId)
+  md.save()
 }
 
 
