af4b243f436357485779605143e8a8e4b50c7b93
DevCryptoDude <devcryptodude@gmail.com>
2021-12-20 18:09:34 +0100

    enhance GRT

--- src/nft/mappings/nft.ts
+++ src/nft/mappings/nft.ts
@@ -1,10 +1,9 @@
 /* eslint-disable prefer-const */
-import { BigInt, BigDecimal, store, Address } from '@graphprotocol/graph-ts'
+import { log, BigInt, BigDecimal, store, Address } from '@graphprotocol/graph-ts'
 import {
- NFTContract,
- METADATA
+ NFTContract
 } from '../../../generated/schema'
-import { NFT, Mint, NameChange, Transfer, ResetStain, EmitRand} from '../../../generated/NFT/NFT'
+import { NFT, Mint, NameChange, Transfer, ResetStain, SetStain, EmitRand} from '../../../generated/NFT/NFT'
 
 let ZERO_BI = BigInt.fromI32(0);
 let ONE_BI = BigInt.fromI32(1);
@@ -26,16 +25,8 @@ export function handleNameChange(event: NameChange): void {
   name.tokenId = BigInt.fromI32(event.params.tokenId.toI32())
   name.owner = NFTC.ownerOf(name.tokenId)
   name.metadata = NFTC.metadata(name.tokenId)
-
-  name.Stain = NFTC.getStain(name.tokenId)
-  name.named = NFTC.getnamed(name.tokenId)
-  name.mef = NFTC.getmef(name.tokenId)
-
+  name.number = NFTC.randomNumberID(name.tokenId)
   name.save()
-
-  let md = new METADATA(event.params.tokenId.toString())
-  md.number = NFTC.randomNumberID(name.tokenId)
-  md.save()
 
 }
 
@@ -53,18 +44,30 @@ export function handleResetStain(event: ResetStain): void {
   name.tokenId = BigInt.fromI32(event.params.tokenId.toI32())
   name.metadata = NFTC.metadata(name.tokenId)
   name.owner = NFTC.ownerOf(name.tokenId)
+  name.number = NFTC.randomNumberID(name.tokenId)
 
-  name.Stain = NFTC.getStain(name.tokenId)
-  name.named = NFTC.getnamed(name.tokenId)
-  name.mef = NFTC.getmef(name.tokenId)
+  name.save()
+}
 
+
+export function handleSetStain(event: SetStain): void {
+
+  let NFTC = getNFTAddress(event.address)
+
+  let timestamp = event.block.timestamp.toI32()
+
+  let name = new NFTContract(event.params.tokenId.toString())
+
+  name.date = BigInt.fromI32(timestamp)
+  name.tokenId = BigInt.fromI32(event.params.tokenId.toI32())
+  name.metadata = NFTC.metadata(name.tokenId)
+  name.owner = NFTC.ownerOf(name.tokenId)
+  name.number = NFTC.randomNumberID(name.tokenId)
 
   name.save()
 
-  let md = new METADATA(event.params.tokenId.toString())
-  md.number = NFTC.randomNumberID(name.tokenId)
-  md.save()
 }
+
 
 export function handleTransfer(event: Transfer): void {
 
@@ -77,16 +80,36 @@ export function handleTransfer(event: Transfer): void {
   name.tokenId = BigInt.fromI32(event.params.tokenId.toI32())
   name.metadata = NFTC.metadata(name.tokenId)
   name.owner = NFTC.ownerOf(name.tokenId)
-
-  name.Stain = NFTC.getStain(name.tokenId)
-  name.named = NFTC.getnamed(name.tokenId)
-  name.mef = NFTC.getmef(name.tokenId)
-
+  name.number = NFTC.randomNumberID(name.tokenId)
   name.save()
 
-  let md = new METADATA(event.params.tokenId.toString())
-  md.number = NFTC.randomNumberID(name.tokenId)
-  md.save()
+}
+
+
+
+export function handleEmitRand(event: EmitRand): void {
+
+  let NFTC = getNFTAddress(event.address)
+
+  let totalSupply = NFTC.totalSupply().toString()
+
+  let timestamp = event.block.timestamp.toI32()
+
+log.info('Message to be displayed: {},  ,', [totalSupply])
+
+   for (var i = 1; i < parseInt(totalSupply)+1; i++) {
+      let TOI = BigInt.fromI32(i);
+
+      log.info('Message to be displayed tokenID: {},  ,', [ i.toString()])
+      let name = new NFTContract(TOI.toString())
+
+      name.date = BigInt.fromI32(timestamp)
+      name.tokenId = TOI
+      name.metadata = NFTC.metadata(name.tokenId)
+      name.owner = NFTC.ownerOf(name.tokenId)
+      name.metadata = NFTC.metadata(name.tokenId)
+      name.save()
+    } 
 }
 
 
